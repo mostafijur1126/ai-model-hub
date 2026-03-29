54929b5965d07a1285affbc9ae1e6c596d41f517
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-03-29 09:16:06 +0600

    add badge in card

--- src/Component/HomePage/AiModels.jsx
+++ src/Component/HomePage/AiModels.jsx
@@ -1,8 +1,24 @@
 import React, { use } from 'react';
+import { FaFire, FaHeart, FaRegSadTear, FaRegStar } from "react-icons/fa";
 
 const AiModels = ({ modelResponse }) => {
     const models = use(modelResponse);
     console.log(models)
+
+    const statusConfig = {
+        popular: {
+            color: "bg-[#E7000B]",
+            icon: <FaFire className='text-orange-300' />
+        },
+        favourite: {
+            color: "bg-[#FF6900]",
+            icon: <FaHeart className='text-red-500' />
+        },
+        mostwanted: {
+            color: "bg-[#FE9A00]",
+            icon: <FaRegStar />
+        }
+    }
     return (
         <div>
             <div className='w-8/12 mx-auto'>
@@ -16,9 +32,16 @@ const AiModels = ({ modelResponse }) => {
                 </div>
                 <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
                     {models.map((model) => {
+                        const current = statusConfig[model.status] || {
+                            color: "bg-gray-400",
+                            icon: <FaRegSadTear />
+                        };
                         return <div key={model.id} className='border border-zinc-300 rounded-3xl overflow-hidden'>
-                            <div className='bg-zinc-200 p-8 flex items-center justify-center h-60'>
+                            <div className='bg-zinc-200 p-8 flex items-center justify-center h-60 relative'>
                                 <img className='w-50' src={model.image} alt="" />
+                                <div className={`badge ${current.color} border-none text-white absolute top-5 rounded-full right-5`}>
+                                    {current.icon}
+                                    { model.status?.toUpperCase()}</div>
                             </div>
                             <div className='p-5 space-y-5'>
                                 <h1 className='text-2xl font-bold'>{model.title}</h1>
