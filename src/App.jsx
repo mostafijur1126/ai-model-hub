8ae74c4c01b542f1c25687577e39dd5955e54617
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-03-29 07:50:49 +0600

    add ai models api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 
+
 import './App.css'
 import Footer from './Component/Footer'
+import AiModels from './Component/HomePage/AiModels'
 import Banner from './Component/HomePage/banner'
 import Navbar from './Component/Navbar'
+
+async function handelModelData() {
+  const response = await fetch('/modelData.json')
+  const data =await response.json();
+  return data
+}
+const modelResponse = handelModelData();
 
 function App() {
 
@@ -10,6 +19,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <AiModels modelResponse={modelResponse}></AiModels>
       <Footer></Footer>
     </>
   )
---
+++ src/Component/HomePage/AiModels.jsx
@@ -0,0 +1,37 @@
+import React, { use } from 'react';
+
+const AiModels = ({ modelResponse }) => {
+    const models = use(modelResponse);
+    console.log(models)
+    return (
+        <div>
+            <div className='w-8/12 mx-auto'>
+                <div className='text-center'>
+                    <button className='btn bg-linear-to-r from-pink-500 to-red-500 text-white rounded-full w-50'>Models</button>
+                    <button className=' w-50 text-gray-400 hover:text-gray-900 ml-5'>Cart(0)</button>
+                </div>
+                <div className='my-10 text-center'>
+                    <h1 className='text-5xl font-bold'>Choose Your AI Model</h1>
+                    <p className='text-xl text-gray-500'>One subscription gives you access to all frontier AI models</p>
+                </div>
+                <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                    {models.map((model) => {
+                        return <div>
+                            <div>
+                                <img className='w-50' src={model.image} alt="" />
+                            </div>
+                            <div>
+                                <h1>{model.title}</h1>
+                                <p>{model.description}</p>
+                                <p>${model.price}/month</p>
+                                <button>Subscribe Now</button>
+                            </div>
+                        </div>
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AiModels;
